2755824d1665825e2c30aac0f02107fda18930a1
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2015-04-03 11:41:28 +0300

    replace odac of zeosdbo

--- QCRollingMillService/logging.pas
+++ QCRollingMillService/logging.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, OoMisc;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
 
 function SaveLog(InData: string): bool;
 
