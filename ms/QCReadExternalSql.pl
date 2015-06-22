40cd4ec9ef6aa9a1cf87ed7ead2f7b961d82c5e6
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2015-06-22 11:42:50 +0300

    create one execution

--- ms/QCReadExternalSql.pl
+++ ms/QCReadExternalSql.pl
@@ -134,7 +134,7 @@ package CONF;{
 			#$self->{$name}->{password} = $config->{$name}->{password};
 		}
 
-		if ($_ =~ /mc/){
+		if ($_ =~ /rm/){
 			$self->{$_}->{host} = $config->{$_}->{firebird}->{host};
 			$self->{$_}->{database} = $config->{$_}->{firebird}->{database};
 			$self->{$_}->{database1} = $config->{$_}->{firebird}->{database1};
@@ -147,7 +147,7 @@ package CONF;{
   
   sub get_conf {
     my($self, $name) = @_; # ссылка на объект
-	my ($mssql, $mc);
+	my ($mssql, $rm);
 
 	if ($name =~ /mssql/){
 		$mssql->{host} = $self->{$name}->{host};
@@ -157,14 +157,14 @@ package CONF;{
 		return $mssql;
 	}
 
-	if ($name =~ /mc/){
-		$mc->{host} = $self->{$name}->{host};
-		$mc->{database} = $self->{$name}->{database};
-		$mc->{database1} = $self->{$name}->{database1};
-		$mc->{dialect} = $self->{$name}->{dialect};
-		$mc->{username} = $self->{$name}->{username};
-		$mc->{password} = $self->{$name}->{password};
-		return $mc;
+	if ($name =~ /rm/){
+		$rm->{host} = $self->{$name}->{host};
+		$rm->{database} = $self->{$name}->{database};
+		$rm->{database1} = $self->{$name}->{database1};
+		$rm->{dialect} = $self->{$name}->{dialect};
+		$rm->{username} = $self->{$name}->{username};
+		$rm->{password} = $self->{$name}->{password};
+		return $rm;
 	}
   } 
 }
@@ -220,7 +220,7 @@ package firebird;{
   sub get_table {
     my($self, $rolling_mill) = @_; # ссылка на объект
 	
-	my($sth, $ref, $query, %values);
+	my($sth, $ref, $query, %values, $table, $heat_table);
 	
 	$query = 'select begindt, NOPLAV as heat, ';
 	$query .= 'MARKA as grade, KLASS as standard ';
@@ -231,16 +231,29 @@ package firebird;{
 	unless($@) {
 		$sth->execute();
 		while ($ref = $sth->fetchrow_hashref()) {
-			my $timestamp = Time::Piece->strptime($ref->{'BEGINDT'}, "%e/%d/%Y %l:%M:%S %p");
-			$ref->{'HEAT'} =~ s/-/_/g;
+			my $timestamp = Time::Piece->strptime($ref->{'BEGINDT'}, "%m/%d/%Y %l:%M:%S %p");
+			$heat_table = $ref->{'HEAT'};
+			$heat_table =~ s/-/_/g;
 #			print(join "\t", 'PARAMS'.$ref->{'HEAT'}, $ref->{'GRADE'}, $ref->{'STANDARD'},
 #							 $ref->{'BEGINDT'}, $timestamp->epoch, "\n");
+
+			# rolling mill table format
+			# 1 -> PARAMSheat -> PARAMS112345
+			# 3 -> PyymmddNheat -> P150231N112345
+			if ($rolling_mill =~ /rm1/) {
+				$table = "PARAMS". $heat_table;
+				$self->{rolling_mill} = $rolling_mill;
+			} elsif ($rolling_mill =~ /rm3/) {
+				$table = "P". $timestamp->strftime("%y%m%d") ."N$heat_table";
+				$self->{rolling_mill} = $rolling_mill;
+			}
 
 			%values = ( 'tid' => $timestamp->epoch,
 						'timestamp' => $timestamp->epoch,
 						'heat' => $ref->{'HEAT'},
 						'grade' => $ref->{'GRADE'},
-						'standard' => $ref->{'STANDARD'}
+						'standard' => $ref->{'STANDARD'},
+						'table' => $table
 					);
 		}
 	} else { $self->{error} = 1; }
@@ -248,20 +261,20 @@ package firebird;{
   }
 
   sub get_values {
-    my($self, $heat) = @_; # ссылка на объект
+    my($self, $table) = @_; # ссылка на объект
 	
 	my($sth, $ref, $query, %values, $TempLeft, $TempRight,
 	   $SectionLeft, $SectionRight, $StrengthClassLeft, $StrengthClassRight);
-	
-	$query = "SELECT * FROM PARAMS$heat ";
-	$query .= "where recordid=(select max(recordid) FROM PARAMS$heat)";
+
+	$query = "SELECT * FROM $table ";
+	$query .= "where recordid=(select max(recordid) FROM $table)";
 
 	eval{ $sth = $self->{dbh}->prepare($query) || die $self->{log}->save(2, "Couldn't execute statement: " . $sth->errstr); };# обработка ошибки
 
 	unless($@) {
 		$sth->execute();
 		while ($ref = $sth->fetchrow_hashref()) {
-			#print(join "\t", $ref->{'RECORDID'}, $ref->{'SECTIONL'}, $ref->{'SECTIONR'}, $ref->{'TOTPL'}, $ref->{'TOTPP'}, "\n");
+			if ($self->{rolling_mill} =~ /rm1/ ) {
 			($SectionLeft,  $StrengthClassLeft) = split('_', uc $ref->{'SECTIONL'});
 			($SectionRight,  $StrengthClassRight) = split('_', uc $ref->{'SECTIONR'});
 			if (250 < $ref->{'TOTPL'}) {
@@ -290,6 +303,39 @@ package firebird;{
 								    'temp' => $TempRight
 								}
 					);
+			}
+
+			if ($self->{rolling_mill} =~ /rm3/ ) {
+				($SectionLeft, $StrengthClassLeft) = split('_', uc $ref->{'SECL'});
+				($SectionRight, $StrengthClassRight) = split('_', uc $ref->{'SECR'});
+				if (250 < $ref->{'TMOUTL'}) {
+					$TempLeft = $ref->{'TMOUTL'};
+				} else {
+					$TempLeft = 0;
+				}
+				if (250 < $ref->{'TMOUTR'}) {
+					$TempRight = $ref->{'TMOUTR'};
+				} else {
+					$TempRight = 0;
+				}
+
+				$StrengthClassLeft =~ s/\x0//g; #удаляем NIL символы
+				$StrengthClassRight =~ s/\x0//g; #удаляем NIL символы
+
+				# left = 0, right = 1
+				%values = ( '0' => {'recordid' 		 => $ref->{'RECORDID'},
+									'section' 		 => $SectionLeft,
+									'strength_class' => $StrengthClassLeft,
+									'temp' 			 => $TempLeft,
+									},
+							'1' => {'recordid' 		 => $ref->{'RECORDID'},
+									'section' 		 => $SectionRight,
+									'strength_class' => $StrengthClassRight,
+									'temp' 			 => $TempRight,
+									}
+						);
+#				print Dumper(\%values);
+			}
 		}
 	} else { $self->{error} = 1; }
 	return(%values);
@@ -471,7 +517,10 @@ package main;
   my $values = $ms_sql->get_values;
 =cut
   my $thread_count = 1;
-  push @threads, threads->create(\&execute_mc1, 'mc1', $thread_count++);
+    push @threads, threads->create(\&execute, 'rm1', $thread_count++);
+	push @threads, threads->create(\&execute, 'rm3', $thread_count++);
+#  push @threads, threads->create(\&execute_rm1, 'rm1', $thread_count++);
+#  push @threads, threads->create(\&execute_rm3, 'rm3', $thread_count++);  
 =comments
   @table_prefix = get_tables($values, "АЦ1");
   for my $tp (@table_prefix){
@@ -492,17 +541,83 @@ package main;
       $thread->join();
   }
 
-sub execute_mc1 {
+sub execute {
 	$0 =~ m/.*[\/\\]/g;
-	my ($thread) = @_;
+	my ($rolling_mill) = @_;
 	
 	my $log = LOG->new();
-	$log->save(4, "thread -> $thread");
+	$log->save(4, "thread -> $rolling_mill");
 
 	# firebird create object
 	my $fbsql = firebird->new();
-	$fbsql->set_con($conf->get_conf('mc1')->{host}, $conf->get_conf('mc1')->{database},
-					$conf->get_conf('mc1')->{dialect}, $conf->get_conf('mc1')->{username}, $conf->get_conf('mc1')->{password});
+	$fbsql->set_con($conf->get_conf($rolling_mill)->{host}, $conf->get_conf($rolling_mill)->{database},
+					$conf->get_conf($rolling_mill)->{dialect}, $conf->get_conf($rolling_mill)->{username},
+					$conf->get_conf($rolling_mill)->{password});
+
+	# mssql create object
+	my $mssql = mssql->new();
+	$mssql->set_con($conf->get_conf('mssql')->{host}, $conf->get_conf('mssql')->{database});
+
+	my $rm = $rolling_mill;
+	$rm =~ s/(\D*)//g;
+
+	while (1) {
+
+		if($fbsql->get_error() == 1) {
+			$fbsql->conn();
+			$log->save(4, "connected fbsql");
+		}
+		
+		my %heat = $fbsql->get_table($rolling_mill);
+#		print Dumper(\%heat);
+		
+		my %values = $fbsql->get_values($heat{'table'});
+#		print Dumper(\%values);
+		
+#		$log->save(4, "left -> $values{'0'}{'recordid'} \t $values{'0'}{'section'} \t $values{'0'}{'strength_class'} \t $values{'0'}{'temp'}");
+#		$log->save(4, "right -> $values{'1'}{'recordid'} \t $values{'1'}{'section'} \t $values{'1'}{'strength_class'} \t $values{'1'}{'temp'}");
+
+		for (0..1) {
+#			$log->save(4, "start side $_");
+
+			my $side = $_;
+
+			$mssql->mssql_send($heat{'tid'}, $heat{'heat'}, $rm,
+							   $heat{'grade'}, $values{$side}{'strength_class'}, $values{$side}{'section'}, $heat{'standard'},
+							   $side, $values{$side}{'temp'});
+
+#			$log->save(4, "end side $_");
+		}
+		sleep(1);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=pod
+sub execute_rm1 {
+	$0 =~ m/.*[\/\\]/g;
+	my ($rolling_mill) = @_;
+	
+	my $log = LOG->new();
+	$log->save(4, "thread -> $rolling_mill");
+
+	# firebird create object
+	my $fbsql = firebird->new();
+	$fbsql->set_con($conf->get_conf($rolling_mill)->{host}, $conf->get_conf($rolling_mill)->{database},
+					$conf->get_conf($rolling_mill)->{dialect}, $conf->get_conf($rolling_mill)->{username},
+					$conf->get_conf($rolling_mill)->{password});
 
 	# mssql create object
 	my $mssql = mssql->new();
@@ -515,10 +630,10 @@ sub execute_mc1 {
 			$log->save(4, "connected fbsql");
 		}
 		
-		my %heat = $fbsql->get_table('mc1');
+		my %heat = $fbsql->get_table($rolling_mill);
 #		print Dumper(\%heat);
 		
-		my %values = $fbsql->get_values($heat{'heat'});
+		my %values = $fbsql->get_values($heat{'table'});
 #		print Dumper(\%values);
 		
 #		$log->save(4, "left -> $values{'0'}{'recordid'} \t $values{'0'}{'section'} \t $values{'0'}{'strength_class'} \t $values{'0'}{'temp'}");
@@ -539,6 +654,55 @@ sub execute_mc1 {
 	}
 }
 
+
+sub execute_rm3 {
+	$0 =~ m/.*[\/\\]/g;
+	my ($rolling_mill) = @_;
+	
+	my $log = LOG->new();
+	$log->save(4, "thread -> $rolling_mill");
+
+	# firebird create object
+	my $fbsql = firebird->new();
+	$fbsql->set_con($conf->get_conf($rolling_mill)->{host}, $conf->get_conf($rolling_mill)->{database},
+					$conf->get_conf($rolling_mill)->{dialect}, $conf->get_conf($rolling_mill)->{username},
+					$conf->get_conf($rolling_mill)->{password});
+
+	# mssql create object
+	my $mssql = mssql->new();
+	$mssql->set_con($conf->get_conf('mssql')->{host}, $conf->get_conf('mssql')->{database});
+
+	while (1) {
+
+		if($fbsql->get_error() == 1) {
+			$fbsql->conn();
+			$log->save(4, "connected fbsql");
+		}
+		
+		my %heat = $fbsql->get_table($rolling_mill);
+		print Dumper(\%heat);
+		
+		my %values = $fbsql->get_values($heat{'table'});
+		print Dumper(\%values);
+		
+#		$log->save(4, "left -> $values{'0'}{'recordid'} \t $values{'0'}{'section'} \t $values{'0'}{'strength_class'} \t $values{'0'}{'temp'}");
+#		$log->save(4, "right -> $values{'1'}{'recordid'} \t $values{'1'}{'section'} \t $values{'1'}{'strength_class'} \t $values{'1'}{'temp'}");
+
+		for (0..1) {
+#			$log->save(4, "start side $_");
+
+			my $side = $_;
+
+			$mssql->mssql_send($heat{'tid'}, $heat{'heat'}, 3,
+							   $heat{'grade'}, $values{$side}{'strength_class'}, $values{$side}{'section'}, $heat{'standard'},
+							   $side, $values{$side}{'temp'});
+
+#			$log->save(4, "end side $_");
+		}
+		sleep(1);
+	}
+}
+=cut
 
 
 
